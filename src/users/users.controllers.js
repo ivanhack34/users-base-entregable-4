7c50b0d64cecc14a7dd5dcd79508e3518c0130a2
ivanhack34 <ivanpolanco34@gmail.com>
2022-12-07 02:24:45 -0400

    Actualizacion hasta llegar a la parte 4

--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -1,5 +1,6 @@
 const Users = require('../models/users.models')
 const uuid = require('uuid')
+const { hashPassword } = require('../utils/crypto')
 
 const findAllUsers = async () => {
     const data = await Users.findAll()
@@ -22,7 +23,7 @@ const createUser = async (obj) => {
         last_name: obj.last_name,
         user_name: obj.user_name,
         email: obj.email,
-        password: obj.password,
+        password: hashPassword(obj.password),
         age: obj.age,
         country: obj.country
     })
